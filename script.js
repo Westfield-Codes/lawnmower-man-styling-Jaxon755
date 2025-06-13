5743325ce91c9ee64a49553b92869b412ed83f1b
Jaxon755 <jaxon201995@student-schoolsofwestfield.org>
2025-06-13 14:03:55 +0000

    tgdytfg

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function setup(){
-// 7 lines of code, including (not in this order)
-// appendChild
-// createElement
-// getElementByID
-// innerHTML
-}
+
+    let ok = document.createElement("button");
+    ok.innerHTML = "OK";
+    let buttons = document.getElementById("buttons");
+    buttons.appendChild(ok);
+    
+    let cancel = document.createElement("button");
+    cancel.innerHTML = "CANCEL";
+    buttons.appendChild(cancel);
